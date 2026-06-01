4d35086d4f0cdfd3acadbb6e01a1b0c7341d92cc
Ethan Shold <sholdee@gmail.com>
2026-06-01 12:44:20 -0500

    fix: emit active drydock formula url

--- Formula/drydock.rb
+++ Formula/drydock.rb
@@ -4,28 +4,23 @@ class Drydock < Formula
   version "0.1.9"
   license "Apache-2.0"
 
-  on_macos do
-    on_intel do
-      url "https://github.com/sholdee/drydock/releases/download/v#{version}/drydock_darwin-amd64.tar.gz"
-      sha256 "c08625fae2dcb0442c48e67e6ce5333c74f382ed1ed927d8d01fcd0d769e0cdb"
-    end
+  host_cpu = RbConfig::CONFIG.fetch("host_cpu")
+  host_os = RbConfig::CONFIG.fetch("host_os")
 
-    on_arm do
-      url "https://github.com/sholdee/drydock/releases/download/v#{version}/drydock_darwin-arm64.tar.gz"
-      sha256 "04f8498a08236c8628496d6be017095abffbe208b89f73c4688b8651c0ba4939"
-    end
-  end
-
-  on_linux do
-    on_intel do
-      url "https://github.com/sholdee/drydock/releases/download/v#{version}/drydock_linux-amd64.tar.gz"
-      sha256 "f632bac02849366b617edd1005f943d81e1854b79d6146e81789fbe9542afd31"
-    end
-
-    on_arm do
-      url "https://github.com/sholdee/drydock/releases/download/v#{version}/drydock_linux-arm64.tar.gz"
-      sha256 "9116263b0a53402a86617ad1cc754347e7b3f130b9a3a04078e60200c16d6b02"
-    end
+  if host_os.include?("linux") && ["aarch64", "arm64"].include?(host_cpu)
+    url "https://github.com/sholdee/drydock/releases/download/v#{version}/drydock_linux-arm64.tar.gz"
+    sha256 "9116263b0a53402a86617ad1cc754347e7b3f130b9a3a04078e60200c16d6b02"
+  elsif host_os.include?("linux") && ["amd64", "x86_64"].include?(host_cpu)
+    url "https://github.com/sholdee/drydock/releases/download/v#{version}/drydock_linux-amd64.tar.gz"
+    sha256 "f632bac02849366b617edd1005f943d81e1854b79d6146e81789fbe9542afd31"
+  elsif ["aarch64", "arm64"].include?(host_cpu)
+    url "https://github.com/sholdee/drydock/releases/download/v#{version}/drydock_darwin-arm64.tar.gz"
+    sha256 "04f8498a08236c8628496d6be017095abffbe208b89f73c4688b8651c0ba4939"
+  elsif ["amd64", "x86_64"].include?(host_cpu)
+    url "https://github.com/sholdee/drydock/releases/download/v#{version}/drydock_darwin-amd64.tar.gz"
+    sha256 "c08625fae2dcb0442c48e67e6ce5333c74f382ed1ed927d8d01fcd0d769e0cdb"
+  else
+    odie "drydock supports macOS and Linux on amd64 or arm64"
   end
 
   def install
