b3e383ebea60f56347bf87e4bfa15730539355f3
Ethan Shold <sholdee@gmail.com>
2026-06-01 12:38:12 -0500

    chore: add drydock formula

---
+++ Formula/drydock.rb
@@ -0,0 +1,40 @@
+class Drydock < Formula
+  desc "Inspect your Argo CD fleet without getting wet"
+  homepage "https://github.com/sholdee/drydock"
+  version "0.1.9"
+  license "Apache-2.0"
+
+  on_macos do
+    on_intel do
+      url "https://github.com/sholdee/drydock/releases/download/v#{version}/drydock_darwin-amd64.tar.gz"
+      sha256 "c08625fae2dcb0442c48e67e6ce5333c74f382ed1ed927d8d01fcd0d769e0cdb"
+    end
+
+    on_arm do
+      url "https://github.com/sholdee/drydock/releases/download/v#{version}/drydock_darwin-arm64.tar.gz"
+      sha256 "04f8498a08236c8628496d6be017095abffbe208b89f73c4688b8651c0ba4939"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/sholdee/drydock/releases/download/v#{version}/drydock_linux-amd64.tar.gz"
+      sha256 "f632bac02849366b617edd1005f943d81e1854b79d6146e81789fbe9542afd31"
+    end
+
+    on_arm do
+      url "https://github.com/sholdee/drydock/releases/download/v#{version}/drydock_linux-arm64.tar.gz"
+      sha256 "9116263b0a53402a86617ad1cc754347e7b3f130b9a3a04078e60200c16d6b02"
+    end
+  end
+
+  def install
+    bin.install "drydock"
+    generate_completions_from_executable bin/"drydock", "completion"
+  end
+
+  test do
+    assert_match "version: v#{version}", shell_output("#{bin}/drydock version")
+    assert_match "completion", shell_output("#{bin}/drydock completion --help")
+  end
+end
---
+++ README.md
@@ -0,0 +1,21 @@
+# sholdee Homebrew Tap
+
+Homebrew formulae for tools maintained by sholdee.
+
+## drydock
+
+Install:
+
+```bash
+brew install sholdee/tap/drydock
+```
+
+Or tap first:
+
+```bash
+brew tap sholdee/tap
+brew install drydock
+```
+
+drydock releases and formula updates are managed from
+[sholdee/drydock](https://github.com/sholdee/drydock).
